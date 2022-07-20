6046508ffa217a854b69fb1bda50c39bd07f2db9
andypetroff <me@andreypetrov.info>
2022-07-20 04:54:29 -0400

    Added Cloudinary storage

--- config.js
+++ config.js
@@ -1,4 +1,15 @@
 export const env = {
+    app_name: 'Simple MERN Blog API',
+
+    // connections
     mongodb: process.env.MONGODB_URI,
     port: process.env.PORT || 4444,
+
+    // folder name != path
+    upload_folder_name: 'uploads',
+
+    // cloudinary
+    cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+    cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
 }
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import { env } from './config.js';
 
 import fs from 'fs';
 import multer from 'multer';
+import { v2 as cloudinary } from 'cloudinary';
+//import { CloudinaryStorage } from 'multer-storage-cloudinary';
+
 import cors from 'cors';
 import mongoose from 'mongoose';
 
@@ -18,33 +21,76 @@ mongoose
 
 const app = express();
 
+// fn (req, file, cb) {}
 const storage = multer.diskStorage({
     destination: (_, __, cb) => {
-        if (!fs.existsSync('uploads')) {
-            fs.mkdirSync('uploads');
+        if (!fs.existsSync(env.upload_folder_name)) {
+            fs.mkdirSync(env.upload_folder_name);
         }
-
-        cb(null, 'uploads');
+        cb(null, env.upload_folder_name);
     },
     filename: (_, file, cb) => {
         cb(null, file.originalname);
     },
 });
 
-const upload = multer({ storage });
+cloudinary.config({
+    cloud_name: env.cloudinary_cloud_name, 
+    api_key: env.cloudinary_api_key, 
+    api_secret: env.cloudinary_api_secret, 
+    secure: true,
+});
+
+// const storage = new CloudinaryStorage({
+//     cloudinary: cloudinary,
+//     params: {
+//       folder: env.upload_folder_name,
+//       unique_filename: false,
+//     },
+// });
+
+const upload = multer({ 
+    storage: storage,
+    //fileFilter,
+});
 
 app.use(express.json());
 app.use(cors());
-app.use('/uploads', express.static('uploads'));
+app.use('/' + env.upload_folder_name, express.static(env.upload_folder_name));
+
+app.get('/', (req, res) => { res.send(env.app_name) });
 
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
 app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
 
-app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
-    res.json({
-        url: `/uploads/${req.file.originalname}`,
-    });
+app.post('/upload', checkAuth, upload.single('image'), async (req, res) => {
+    try {
+        const result = await cloudinary.uploader.upload(
+            req.file.path, 
+            {
+              use_filename: true,
+              folder: env.upload_folder_name,
+              unique_filename: false,
+              overwrite: true,
+            }
+        );
+
+        fs.unlinkSync(req.file.path);
+
+        res.json({ 
+            url: result.secure_url,
+        });
+    } catch (e) {
+        console.error(e);
+        res.status(403).json({
+            message: 'Failed to upload the file!',
+        });
+    }
+
+    // res.json({ 
+    //     url: req.file.path, 
+    // });
 });
 
 app.get('/tags', PostController.getLastTags);
